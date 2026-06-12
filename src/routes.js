97245a1280658aa4220429bb98091bce735fe296
karim1311 <valenzuelakarim03@gmail.com>
2026-06-11 22:09:28 -0700

    w04 asssignment

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,18 @@
 import express from 'express'
 
 import { showHomePage } from './controllers/index.js'
-import { showOrganizationsPage } from './controllers/organizations.js'
-import { showProjectsPage, showProjectDetailsPage } from './controllers/projects.js'
-import { showCategoriesPage, showCategoryDetailsPage } from './controllers/categories.js'
+import { 
+    showOrganizationsPage, 
+    showOrganizationDetailsPage, 
+    showNewOrganizationForm, 
+    processNewOrganizationForm, 
+    organizationValidation,
+    showEditOrganizationForm,
+    processEditOrganizationForm 
+} from './controllers/organizations.js'
+import { showProjectsPage, showProjectDetailsPage, showNewProjectForm, processNewProjectForm, projectValidation } from './controllers/projects.js'
+import { showCategoriesPage, showCategoryDetailsPage, showAssignCategoriesForm, processAssignCategoriesForm } from './controllers/categories.js'
 import { testErrorPage } from './controllers/errors.js'
-import { showOrganizationDetailsPage } from './controllers/organizations.js'
 
 const router = express.Router()
 
@@ -19,6 +26,22 @@ router.get('/organization/:id', showOrganizationDetailsPage)
 router.get('/project/:id', showProjectDetailsPage)
 // Route for category details page
 router.get('/category/:id', showCategoryDetailsPage)
+// Route for new organization page
+router.get('/new-organization', showNewOrganizationForm)
+// Route to handle new organization form submission
+router.post('/new-organization', organizationValidation, processNewOrganizationForm)
+// Route to display the edit organization form
+router.get('/edit-organization/:id', showEditOrganizationForm)
+// Route to handle the edit organization form submission
+router.post('/edit-organization/:id', organizationValidation, processEditOrganizationForm)
+// Route for new project page
+router.get('/new-project', showNewProjectForm)
+// Route to handle new project form submission
+router.post('/new-project', projectValidation, processNewProjectForm)
+// Routes to handle the assign categories to project form
+router.get('/assign-categories/:projectId', showAssignCategoriesForm)
+router.post('/assign-categories/:projectId', processAssignCategoriesForm)
+
 // error-handling routes
 router.get('/test-error', testErrorPage)
 
